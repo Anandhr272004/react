d08f59e56f3a6ca5b28e3306dd737c7e939ae82f
Anandh <anandh272004@gmail.com>
2024-07-17 10:47:53 +0530

    first commit

---
+++ mydemo/src/Home.jsx
@@ -0,0 +1,14 @@
+
+
+function Home() {
+    return (
+        <div className="Home">
+            <h2>Hello  &nbsp; &nbsp;<span className="hell">Welcome to React JS</span></h2>
+            <div className="nav">
+                <h2>Single Page Web Application</h2>
+            </div>
+        </div>
+
+    )
+}
+export default Home;
